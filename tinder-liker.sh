db1a7c3dc56bb6ca32900861da75bd89863d821b
m0skit0 <ametric@gmail.com>
2020-01-31 20:34:09 +0100

    Added tapping back (it's much faster) but as a separate mode (so swipe is still an option)

--- tinder-liker.sh
+++ tinder-liker.sh
@@ -4,6 +4,7 @@
 # -h  Shows help and exits (remaining switches are ignored)
 # -c  Define X Y coordinates
 # -d  Define delay between likes (default delay recommended)
+# -t  Use tap instead of swipe
 # -g  Go home after Ctrl-C
 
 trap doExit INT
@@ -20,18 +21,46 @@ function setModel() {
 	MODEL=$(adb devices -l | grep -m 1 -i device: | sed -E 's/.*model:(.*?) device.*/\1/')
 }
 
-function setDetectedModelCoordinates() {
-	case $MODEL in
-		# OnePlus 7 Pro
-	    GM1913) 
+function setOnePlus7ProCoordinates() {
+	case $MODE in
+		swipe)
 			X=750
 			Y=1600
 			OFFSET=600;;
+		tap)
+			X=970
+			Y=2945;;
+		*)
+			echo "Unknown mode $MODE"
+	        exit 4
+	esac
+}
+
+function setXiaomiMiMix2Coordinates() {
+	case $MODE in
+		swipe)
+			X=500
+	        Y=1000
+	        OFFSET=500;;
+		*)
+			echo "Unknown mode $MODE"
+	        exit 4
+	esac
+}
+
+function setDetectedModelCoordinates() {
+	case $MODEL in
+
+		# OnePlus 7 Pro
+	    GM1913)
+			setOnePlus7ProCoordinates;;
+
 		# Xiaomi Mi MIX 2
 		Mi_MIX_2)
 			X=500
 	        Y=1000
-	        OFFSET=500;;				
+	        OFFSET=500;;
+
 	    *)
 			echo "Unknown model $1"
 	        exit 3
@@ -41,13 +70,23 @@ function setDetectedModelCoordinates() {
 function showHelp() {
 	echo " Usage: ./tinder-liker.sh [-c X Y] [-d delay] [-g] [-h]"
 	echo -e "\t-h  Shows help and exits (remaining switches are ignored)"
-	echo -e "\t-c  Define X Y coordinates"
+	echo -e "\t-c  Define X Y coordinates (if -t will use it for tapping instead of swipe)"
 	echo -e "\t-d  Define delay between likes (default delay recommended)"
+	echo -e "\t-t  Use tap instead of swipe"
 	echo -e "\t-g  Go home after Ctrl-C"
 }
 
-# Default delay
+function swipe() {
+	END_X=$((X + OFFSET))
+	adb shell input touchscreen swipe $X $Y $END_X $Y
+}
+
+function tap() {
+	adb shell input tap $X $Y
+}
+
 DELAY=0
+MODE="swipe"
 
 # Get params
 while [ "$1" != "" ]; do
@@ -65,6 +104,8 @@ while [ "$1" != "" ]; do
             DELAY=$1;;
         -g)
 			GO_HOME="true";;
+        -t)
+			MODE="tap";;
         *)
 			echo "Unknown parameter $1"
             exit 1
@@ -89,10 +130,18 @@ adb shell am start -n com.tinder/.activities.LoginActivity
 sleep 5
 
 # Swipe like forever
-echo "Swiping... (Ctrl-C to stop and go home)"
+echo "Liking... (Ctrl-C to stop)"
 while true; do
-	END_X=$((X + OFFSET))
-	adb shell input touchscreen swipe $X $Y $END_X $Y
+	case $MODE in
+	    swipe) 
+			swipe;;
+		# Xiaomi Mi MIX 2
+		tap)
+			tap;;				
+	    *)
+			echo "Unknown mode $MODE"
+	        exit 4
+	esac
 	sleep "$DELAY";
 done
 exit 0
